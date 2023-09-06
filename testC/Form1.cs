b8684326f2effca5c50f71a2985cf5bcd6c71607
Maks Krupski <krupski.maxim@gmail.com>
2023-09-06 12:37:37 +0200

    zmiana kierunku

--- testC/Form1.cs
+++ testC/Form1.cs
@@ -9,7 +9,32 @@ namespace testC
 
         private void ButtonClicked(object sender, EventArgs e)
         {
-            Application.Exit();
+            //Application.Exit();
+
+            //szerokoœæ okna aplikacji
+            int windowWidth = Size.Width;
+
+            //wyskoœæ okna aplikacji
+            int windowHeight = Size.Height;
+
+            windowWidth -= button1.Width;
+            windowHeight -= button1.Height;
+
+            //instalizacja generatora liczb losowych 
+            Random rdn = new Random();
+
+            //losowa odleg³oœæ od lewej - nie wiêcej ni¿ szerokoœæ okna
+            int randomLeft = rdn.Next(windowWidth);
+
+
+            int randomTop = rdn.Next(windowHeight);
+
+            //przesuwamy guzik
+            //location musi byæ przekazany jako punkt na ekranie
+            Point topLeftCorner = new Point(randomLeft, randomTop);
+            //centerButton to nazwa (atrybut Name) mojego guzika
+            button1.Location = topLeftCorner;
+
         }
     }
 }
